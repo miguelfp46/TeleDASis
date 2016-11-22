92af08727c60cc56a2983bb7311afb93ac172bd0
Alexis <arodriguezsilvente@gmail.com>
2016-11-22 17:23:12 +0100

    creando la clase usuario

---
+++ TeleDASis/TeleDASis/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleDASis
+{
+    class Usuario
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public String apellidos { get; set; }
+        public int telefono { get; set; }
+        public int telefonofamiliar { get; set; }
+        public int tlfmovil { get; set; }
+        public String tarjetasanitaria { get; set; }
+        public String dni { get; set; }
+        public String vivienda { get; set;}
+        public DateTime fechaentrada { get; set; }
+
+
+    }
+}
