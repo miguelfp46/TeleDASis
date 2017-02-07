cf4be2d57123871fa5fcf981ca8dcf8faa257082
Miguel <miguelmiguelmiguel@gmail.com>
2017-02-07 16:54:55 +0100

    validacion dni OK

--- TeleDASis/TeleDASis/Alta.xaml.cs
+++ TeleDASis/TeleDASis/Alta.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 namespace TeleDASis
 {
 
@@ -41,37 +40,38 @@ namespace TeleDASis
 
         private void btAccept_Click(object sender, RoutedEventArgs e)
         {
-            nombre = tbNombre.Text;
-            apellido = tbApellido.Text;
-            apellido2 = tbApellido2.Text;
-            dni = tbDni.Text;
-            nTelefono = tbTelefono.Text;
-            nTelefonoFamiliar = tbFamiliar.Text;
-            movil = tbMovil.Text;
-            targetaSanitaria = tbTargetaSanitaria.Text;
-            DateTime dt = tbFechaDeAlta.DisplayDate;
-            fechaDeAlta = dt.ToString("yyyy/MM/dd");
-            vivienda = int.Parse(tbVivienda.Text);
-            if (nombre == "" || apellido == "" || apellido2 == "" || dni == "" ||
-                nTelefono == "" || nTelefonoFamiliar == "" || movil == "" || targetaSanitaria == ""
-                || vivienda == 0)
-            {
-                MessageBox.Show("¡Debes rellenar todos los campos!", "Campos vacíos", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            else
-            {
-                if (databaseConnector.instance.addUser(nombre, apellido, apellido2, dni, nTelefono,
-                nTelefonoFamiliar, movil, targetaSanitaria, fechaDeAlta, vivienda) == true)
-                {
-                    MessageBox.Show("Se ha introducido a " + nombre + " con éxito", "Usuario añadido", MessageBoxButton.OK, MessageBoxImage.Information);
-                    this.Close();
-                }
-                else
-                {
-                    MessageBox.Show("No se ha podido añadir a " + nombre + " correctamente.", "Fallo al añadir usuario", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-
+            //nombre = tbNombre.Text;
+            //apellido = tbApellido.Text;
+            //apellido2 = tbApellido2.Text;
+            //dni = tbDni.Text;
+            //nTelefono = tbTelefono.Text;
+            //nTelefonoFamiliar = tbFamiliar.Text;
+            //movil = tbMovil.Text;
+            //targetaSanitaria = tbTargetaSanitaria.Text;
+            //DateTime dt = tbFechaDeAlta.DisplayDate;
+            //fechaDeAlta = dt.ToString("yyyy/MM/dd");
+            //vivienda = int.Parse(tbVivienda.Text);
+            //if (nombre == "" || apellido == "" || apellido2 == "" || dni == "" ||
+            //    nTelefono == "" || nTelefonoFamiliar == "" || movil == "" || targetaSanitaria == ""
+            //    || vivienda == 0)
+            //{
+            //    MessageBox.Show("¡Debes rellenar todos los campos!", "Campos vacíos", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+            //else
+            //{
+            //    if (databaseConnector.instance.addUser(nombre, apellido, apellido2, dni, nTelefono,
+            //    nTelefonoFamiliar, movil, targetaSanitaria, fechaDeAlta, vivienda) == true)
+            //    {
+            //        MessageBox.Show("Se ha introducido a " + nombre + " con éxito", "Usuario añadido", MessageBoxButton.OK, MessageBoxImage.Information);
+            //        this.Close();
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("No se ha podido añadir a " + nombre + " correctamente.", "Fallo al añadir usuario", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    }
+            //}
+            dni = "";
+            MessageBox.Show(Validaciones.validarNIF(dni)?"1":"0");
             
             
             
@@ -98,69 +98,7 @@ namespace TeleDASis
 
             // this.Close();
         }
-        public bool VerificarDni(string dni)
-        {
-            String aux = null;
-            
-            dni = dni.ToUpper();
-
-            //ponemos la letra en mayuscula
-            aux = dni.Substring(0, dni.Length - 1);
-            //quitamos la letra del NIF
-            if (aux.Length >= 7 && this.CadenaEsNumero(aux))
-                aux = this.CalculaNif(aux);//calculamos la letra del nif para comparar con la que tenemos
-            else
-                return false;
-            //comparamos las letras
-            return (dni != aux);
-        }
-
-        private bool CadenaEsNumero(string aux)
-        {
-            for(int i = 0; i > 10; i++)
-            {
-                if (aux.Contains(i.ToString()))
-                {
-                    return true;
-                }
-                else
-                    return false;
-            }
-            
-            throw new NotImplementedException();
-        }
-
-        private String CalculaNif(String strA)
-        {
-            const String cCADENA = "TRWAGMYFPDXBNJZSQVHLCKE";
-            const String cNUMEROS = "0123456789";
-            Int32 a = 0;
-            Int32 b = 0;
-            Int32 c = 0;
-            Int32 NIF = 0;
-            StringBuilder sb = new StringBuilder();
-
-            strA = strA.Trim();
-            if (strA.Length == 0) return "";
-
-            //dejar sólo los números
-            for (int i = 0; i <= strA.Length - 1; i++)
-                if (cNUMEROS.IndexOf(strA[i]) > -1) sb.Append(strA[i]);
-            strA = sb.ToString();
-            a = 0;
-            NIF = Convert.ToInt32(strA);
-            do
-            {
-                b = Convert.ToInt32((NIF / 24));
-                c = NIF - (24 * b);
-                a = a + c;
-                NIF = b;
-            } while (b != 0);
-
-            b = Convert.ToInt32((a / 23));
-            c = a - (23 * b);
-            return strA.ToString() + cCADENA.Substring(c, 1);
-        }
+        
     }
        
 }
