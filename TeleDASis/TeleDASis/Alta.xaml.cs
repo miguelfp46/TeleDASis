e07bca4f59a885ff494f80161bb2a3221dbfcbef
Miguel <miguelmiguelmiguel@gmail.com>
2017-02-07 16:09:40 +0100

    alta cuando hagamos una alta que salte el messagebox y que compruebe que esten todos los campos llenos.

--- TeleDASis/TeleDASis/Alta.xaml.cs
+++ TeleDASis/TeleDASis/Alta.xaml.cs
@@ -41,29 +41,40 @@ namespace TeleDASis
 
         private void btAccept_Click(object sender, RoutedEventArgs e)
         {
-            if (VerificarDni(dni) == true)
+            nombre = tbNombre.Text;
+            apellido = tbApellido.Text;
+            apellido2 = tbApellido2.Text;
+            dni = tbDni.Text;
+            nTelefono = tbTelefono.Text;
+            nTelefonoFamiliar = tbFamiliar.Text;
+            movil = tbMovil.Text;
+            targetaSanitaria = tbTargetaSanitaria.Text;
+            DateTime dt = tbFechaDeAlta.DisplayDate;
+            fechaDeAlta = dt.ToString("yyyy/MM/dd");
+            vivienda = int.Parse(tbVivienda.Text);
+            if (nombre == "" || apellido == "" || apellido2 == "" || dni == "" ||
+                nTelefono == "" || nTelefonoFamiliar == "" || movil == "" || targetaSanitaria == ""
+                || vivienda == 0)
             {
-                MessageBox.Show("Okey");
-
-            } else
-            {
-                MessageBox.Show("Algo no rula bien.");
+                MessageBox.Show("¡Debes rellenar todos los campos!", "Campos vacíos", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            else
+            {
+                if (databaseConnector.instance.addUser(nombre, apellido, apellido2, dni, nTelefono,
+                nTelefonoFamiliar, movil, targetaSanitaria, fechaDeAlta, vivienda) == true)
+                {
+                    MessageBox.Show("Se ha introducido a " + nombre + " con éxito", "Usuario añadido", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No se ha podido añadir a " + nombre + " correctamente.", "Fallo al añadir usuario", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
             
-            //nombre = tbNombre.Text;
-            //apellido = tbApellido.Text;
-            //apellido2 = tbApellido2.Text;
-            //dni = tbDni.Text;
-            //nTelefono = tbTelefono.Text;
-            //nTelefonoFamiliar = tbFamiliar.Text;
-            //movil = tbMovil.Text;
-            //targetaSanitaria = tbTargetaSanitaria.Text;
-            //DateTime dt = tbFechaDeAlta.DisplayDate;
-            //fechaDeAlta = dt.ToString("yyyy/MM/dd");
-            //vivienda = int.Parse(tbVivienda.Text);
-            //databaseConnector.instance.addUser(nombre, apellido, apellido2, dni, nTelefono,
-            //    nTelefonoFamiliar, movil, targetaSanitaria, fechaDeAlta, vivienda);
-            //this.Close();
+            
+            
         }
         private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
