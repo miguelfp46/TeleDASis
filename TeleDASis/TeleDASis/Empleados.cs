93ca28411374b71c66a54b35a5104f745f6705f2
Miguel <miguelmiguelmiguel@gmail.com>
2017-02-28 15:27:22 +0100

    baja cuando confirmamos baja sin un dni salta un message box + clase empleado creada.

---
+++ TeleDASis/TeleDASis/Empleados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleDASis
+{
+    class Empleados
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string nombreUsuario { get; set; }
+        public string primerApellido { get; set; }
+        public string segundoApellido { get; set; }
+        public int tlfmovil { get; set; }
+        public string dni { get; set; }
+        public string fechaAlta { get; set; }
+        public string fechaBaja { get; set; }
+        public string motivodeBaja { get; set; }
+        public Empleados(string nombre, string nombreUsuario,int movil, string dni, string fechaDeAlta, string apellido, string apellido2)
+        {
+            this.nombre = nombre;
+            this.nombreUsuario = nombreUsuario;
+            this.tlfmovil = movil;
+            this.dni = dni;
+            this.fechaAlta = fechaDeAlta;
+            this.primerApellido = apellido;
+            this.segundoApellido = apellido2;
+
+        }
+    }
+}
