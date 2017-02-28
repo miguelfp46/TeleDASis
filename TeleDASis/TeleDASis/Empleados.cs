d8b6f66ad6f33b36d82bd842cb2595f8851085d6
Miguel <miguelmiguelmiguel@gmail.com>
2017-02-28 16:00:20 +0100

    cambios

--- TeleDASis/TeleDASis/Empleados.cs
+++ TeleDASis/TeleDASis/Empleados.cs
@@ -11,6 +11,7 @@ namespace TeleDASis
         public int id { get; set; }
         public string nombre { get; set; }
         public string nombreUsuario { get; set; }
+        public string password { get; set; }
         public string primerApellido { get; set; }
         public string segundoApellido { get; set; }
         public int tlfmovil { get; set; }
@@ -18,10 +19,11 @@ namespace TeleDASis
         public string fechaAlta { get; set; }
         public string fechaBaja { get; set; }
         public string motivodeBaja { get; set; }
-        public Empleados(string nombre, string nombreUsuario,int movil, string dni, string fechaDeAlta, string apellido, string apellido2)
+        public Empleados(string nombre, string nombreUsuario, string password, int movil, string dni, string fechaDeAlta, string apellido, string apellido2)
         {
             this.nombre = nombre;
             this.nombreUsuario = nombreUsuario;
+            this.password = password;
             this.tlfmovil = movil;
             this.dni = dni;
             this.fechaAlta = fechaDeAlta;
